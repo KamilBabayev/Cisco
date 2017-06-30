dfcd7f03277732a2988a6923f25db8c970923869
Kamil Babayev <kamilyunis@gmail.com>
2017-06-30 15:27:36 +0000

    web application develoing stae

--- webapp/server.py
+++ webapp/server.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'admin123':
+            return render_template('main.html')
+        else:
+            return render_template('authfail.html')
+	
+		
+	
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
