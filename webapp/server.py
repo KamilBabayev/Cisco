bb3df4a82f6840e0fe7970a650a4612ddcef0778
Kamil Babayev <kamilyunis@gmail.com>
2017-06-30 08:35:17 +0000

    flask web app

--- webapp/server.py
+++ webapp/server.py
@@ -4,6 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
-	return "welcome"
+    return "welcome"
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=80)
