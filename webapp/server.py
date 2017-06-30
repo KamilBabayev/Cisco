d9726d68de55c98e0c636f9d255544e5e5c75f59
Kamil Babayev <kamilyunis@gmail.com>
2017-06-30 12:19:08 +0000

    templates directory added

--- webapp/server.py
+++ webapp/server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
 from flask import Flask
+from flask import request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "welcome"
+	return render_template("index.html")
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
