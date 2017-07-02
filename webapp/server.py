cc2def8bf4d339058e0618d5573160c9666a0e88
Kamil Babayev <kamilyunis@gmail.com>
2017-07-02 07:35:47 +0000

    initial web app structure is ready without css

--- webapp/server.py
+++ webapp/server.py
@@ -8,18 +8,27 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'GET':
+       return redirect('/')
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if username == 'admin' and password == 'admin123':
-            return render_template('main.html')
+             return redirect(url_for('main'))
         else:
             return render_template('authfail.html')
-	
-		
-	
+
+@app.route('/main')
+def main():
+	return render_template('main.html')
+@app.route('/addnewdevice')
+def addnewdevice():
+    return render_template('addnewdevice.html')
+@app.route('/addnewcommand')
+def addnewcommand():
+    return render_template('addnewcommand.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
